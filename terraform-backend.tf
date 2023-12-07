4bdfc548bf3f02b6829883825a1b894f934321d1
anand <justformyclass2@gmail.com>
2023-12-07 07:07:06 +0000

    keypair created

--- terraform-backend.tf
+++ terraform-backend.tf
@@ -1,8 +1,8 @@
 terraform {
 
-	backend "s3" {
-		bucket = "terraform.redmi-prod1.project"
-		key = "terraform.tfstate"
-		region = "us-east-1"
-		}
+  backend "s3" {
+    bucket = "terraform.redmi-prod1.project"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
